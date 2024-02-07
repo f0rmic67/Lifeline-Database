48402ebdc4c85e1a745d83c57dfb77821db9c9a7
Anthony91501 <90730954+Anthony91501@users.noreply.github.com>
2024-02-07 16:51:53 -0500

    Update Lifelinedb.sql
    added username field to student user table 
    added pk to student medical info table 
    added backsticks to password

--- Lifelinedb.sql
+++ Lifelinedb.sql
@@ -3,11 +3,13 @@ CREATE TABLE student_user (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password VARCHAR(30),
+    `password` VARCHAR(30), -- password is a reserved word in sql 
+    username VARCHAR(30),
     PRIMARY KEY (student_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE student_medical_info (
+    id INT UNSIGNED NOT NULL AUTO-INCREMENT -- for uniqueness 
     student_id BIGINT UNSIGNED NOT NULL,
     dob DATE,
     heart_problems BOOL,
@@ -29,11 +31,11 @@ CREATE TABLE student_medical_info (
     allergies VARCHAR(100),
     other VARCHAR(100),
     last_update DATE,
-    PRIMARY KEY (student_id), -- Primary key is foreign key to student_id since there is a one-to-zero-or-one relationship
+    PRIMARY KEY (id), -- uniqueness-- Primary key is foreign key to student_id since there is a one-to-zero-or-one relationship
     FOREIGN KEY (student_id)
         REFERENCES student_user(student_id)
         ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4;
 
 CREATE TABLE emergency_contacts (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT, -- Separate id is used since a student can have more than one emergency contact
